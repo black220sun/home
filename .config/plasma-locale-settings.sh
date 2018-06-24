fd2b15991b91e77ab539adb3a86d47d11f7fe137
Vladislav Shulepov <shulepov.vlad@gmail.com>
2018-06-24 09:05:09 +0300

    .config

---
+++ .config/plasma-locale-settings.sh
@@ -0,0 +1,9 @@
+# Generated script, do not edit
+# Exports language-format specific env vars from startkde.
+# This script has been generated from kcmshell5 formats.
+# It will automatically be overwritten from there.
+export LANG=en_US.UTF-8
+export LC_NUMERIC=uk_UA.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_MONETARY=uk_UA.UTF-8
+export LC_MEASUREMENT=uk_UA.UTF-8
