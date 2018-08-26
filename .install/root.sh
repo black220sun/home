78e88851fb979aa267c982f07f097647dbe9ed33
Vladislav Shulepov <shulepov.vlad@gmail.com>
2018-08-26 10:20:21 +0300

    fixes for .install

--- .install/root.sh
+++ .install/root.sh
@@ -1,4 +1,4 @@
-ln -sf ~/.zshrc /root/.zshrc
-ln -sf ~/.aliases /root/.aliases
+$SUDO ln -sf ~/.zshrc /root/.zshrc
+$SUDO ln -sf ~/.aliases /root/.aliases
 chsh root -s /bin/zsh
 chsh $USER -s /bin/zsh
