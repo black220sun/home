32c41057e6860324d86270f5c6f85d799e59d14e
Vladislav Shulepov <shulepov.vlad@gmail.com>
2018-10-05 23:38:36 +0300

    removed counters

--- Music/init.sh
+++ Music/init.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-declare -i total=0
-declare -i downloaded=0
-declare -i skipped=0
-declare -i failed=0
 for dir in $HOME/Music/*/
 do
   cd "$dir"
@@ -11,15 +7,9 @@ do
 	read -r link
 	if [ -f "$file.mp3" ]; then
 	  echo "$file.mp3 is already in library. Skipping..."
-	  skipped+=1
 	else
-	  wget -O "$file.mp3" $link && downloaded+=1 || failed+=1
+	  wget -O "$file.mp3" $link
 	fi
-	total+=1
   done
   cd ..
 done
-echo "Processed $total files:"
-echo "  $downloaded downloaded"
-echo "  $failed failed"
-echo "  $skipped skipped"
