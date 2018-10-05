d54783669a0af9c1e17055b0cec8fcbba34d9e8e
Vladislav Shulepov <shulepov.vlad@gmail.com>
2018-10-05 23:18:30 +0300

    tracks added. scripts reworked

--- Music/init.sh
+++ Music/init.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
-for dir in ./*/
+declare -i total=0
+declare -i downloaded=0
+declare -i skipped=0
+declare -i failed=0
+for dir in $HOME/Music/*/
 do
-  cd $dir
+  cd "$dir"
   cat .tracks | while read -r file ;
   do
 	read -r link
-	wget -O "$file.mp3" $link
+	if [ -f "$file.mp3" ]; then
+	  echo "$file.mp3 is already in library. Skipping..."
+	  skipped+=1
+	else
+	  wget -O "$file.mp3" $link && downloaded+=1 || failed+=1
+	fi
+	total+=1
   done
   cd ..
 done
+echo "Processed $total files:"
+echo "  $downloaded downloaded"
+echo "  $failed failed"
+echo "  $skipped skipped"
---
+++ Music/stat.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+artists=`/bin/ls -1BI '*.sh' | wc -l`
+present=`/bin/ls -1B */* | wc -l`
+total=`cat */.tracks | wc -l | xargs -I {} expr {} / 2`
+echo "Library:"
+echo "  $artists artists"
+echo "  $total tracks listed"
+echo "  $present tracks downloaded"
